540deffa80d0fc5172761e244fc59cfcf4e5a54a
Megan Yin <my474@cornell.edu>
2020-05-20 17:33:47 -0400

    Finish landing page (#10)
    * finish landing page
    
    * remove tests; static headshot size

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import Landing from './Landing';
+import Fonts from './Fonts';
+
+library.add(faChevronDown);
 
 export default (): React.ReactElement => (
-  <div className="App" />
+  <div className="App">
+    <Fonts />
+    <Landing />
+  </div>
 );
