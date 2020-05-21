37c39929b717bdd7b203490d237d2ce211cac57d
Megan Yin <my474@cornell.edu>
2020-05-21 16:32:39 -0400

    add confetti (#12)
    important stuff

--- src/Landing/ChevronDown.tsx
+++ src/Landing/ChevronDown.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { animateScroll } from 'react-scroll';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import styles from './ChevronDown.module.css';
 
-export default (): React.ReactElement => (
-  <button
-    type="button"
-    className={styles.chevron}
-  >
-    <FontAwesomeIcon icon={faChevronDown} />
-  </button>
-
-);
+export default (): React.ReactElement => {
+  const scrollDown = (): void => {
+    animateScroll.scrollTo(document.documentElement.clientHeight, {
+      delay: 100,
+      duration: 400,
+      smooth: true,
+    });
+  };
+  return (
+    <button
+      type="button"
+      className={styles.chevron}
+      onClick={scrollDown}
+    >
+      <FontAwesomeIcon icon={faChevronDown} />
+    </button>
+  );
+};
