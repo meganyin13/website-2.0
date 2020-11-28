4304995ae8b6c0bd415ad6268c2dc07e90d091de
Sam Zhou <sam@developersam.com>
2020-11-28 18:57:28 -0500

    Unbreak devmegan favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ const App = (props: AppProps): ReactElement => {
     <>
       <Head>
         <meta charSet="utf-8" />
-        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
         <meta name="description" content="Megan Yin's website" />
