ecf133c26a9a8f2e80cc2455168ff61a0eaf76b6
Megan Yin <my474@cornell.edu>
2020-11-28 17:19:34 -0500

    fix chevron size and remove Fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ const App = (props: AppProps): ReactElement => {
   return (
     <>
       <Head>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
