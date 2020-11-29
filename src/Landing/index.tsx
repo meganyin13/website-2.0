9f30eedf59e148ef8b50021e9d019f3fe5c640ec
Megan Yin <my474@cornell.edu>
2020-11-29 17:20:16 -0500

    Use next-images to serve images from any folder

--- src/Landing/index.tsx
+++ src/Landing/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ChevronDown from './ChevronDown';
 import styles from './index.module.css';
+import headshot from './headshot.jpg';
 
 const Landing = (): React.ReactElement => (
   <div className={styles.landing}>
-    <img className={styles.headshot} src="/img/headshot.jpg" alt="Megan Yin" />
+    <img className={styles.headshot} src={headshot} alt="Megan Yin" />
     <div className={styles.text}>
       <h1>Megan Yin</h1>
       <h3>Computer Science @ Cornell University</h3>
